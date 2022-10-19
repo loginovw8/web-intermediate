38234088e9446c37c1aa115cf07e245d51a2421b
loginovw8 <loginovw8@gmail.com>
2022-10-19 21:01:35 +0300

    Lesson 3 modified.

--- 03-sass-js-bundle/backend/app.js
+++ 03-sass-js-bundle/backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mysql = require("mysql");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 // Соединение с базой данных
 const connection = mysql.createConnection({
