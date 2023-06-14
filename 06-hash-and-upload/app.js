fb41824a6a33c4a5cb9105806430de61b5c13c72
loginovw8 <loginovw8@gmail.com>
2023-06-14 12:16:19 +0300

    Lesson 5 and 6 edited.

--- 06-hash-and-upload/app.js
+++ 06-hash-and-upload/app.js
@@ -29,39 +29,39 @@ app.listen(3000);
  * Маршруты
  */
 app.get("/", (req, res) => {
-  res.render("home");
+    res.render("home");
 
-  // Пароль пользователя
-  let password = "12345";
+    // Пароль пользователя
+    let password = "12345";
 
-  // Соль
-  // https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D1%8C_(%D0%BA%D1%80%D0%B8%D0%BF%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F)
-  let salt = 10;
+    // Соль
+    // https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D1%8C_(%D0%BA%D1%80%D0%B8%D0%BF%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F)
+    let salt = 10;
 
-  // Шифруем и записываем в БД
-  bcrypt.hash(password, salt, (err, hash) => {
-    console.log(hash);
-  });
+    // Шифруем и записываем в БД
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
 
-  // Извлеченный из БД хэш
-  let hash = "$2b$10$g8QYamUl7dDVg3ZQj5z7duMZMrB4kwSCqEBVXknCxcOpxmZDk524q";
+    // Извлеченный из БД хэш
+    let hash = "$2b$10$g8QYamUl7dDVg3ZQj5z7duMZMrB4kwSCqEBVXknCxcOpxmZDk524q";
 
-  // Сравниваем хэш и пользовательский пароль
-  bcrypt.compare(password, hash, (err, result) => {
-    console.log(result);
-  });
+    // Сравниваем хэш и пользовательский пароль
+    bcrypt.compare(password, hash, (err, result) => {
+        console.log(result);
+    });
 });
 
 app.post("/upload", upload.single("image"), (req, res) => {
-  const tempPath = req.file.path;
-  const targetPath = path.join(
-    __dirname,
-    "./public/img/" + req.file.originalname
-  );
+    const tempPath = req.file.path;
+    const targetPath = path.join(
+        __dirname,
+        "./public/img/" + req.file.originalname
+    );
 
-  fs.rename(tempPath, targetPath, (err) => {
-    if (err) console.log(err);
-    
-    res.redirect('/');
-  });
+    fs.rename(tempPath, targetPath, (err) => {
+        if (err) console.log(err);
+
+        res.redirect('/');
+    });
 });
