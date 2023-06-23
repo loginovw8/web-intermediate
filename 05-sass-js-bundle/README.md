0a9a818c908923286fcfdb2820e04444a4f7feb2
loginovw8 <loginovw8@gmail.com>
2023-06-23 20:20:56 +0300

    Lesson 5 edited.

--- 05-sass-js-bundle/README.md
+++ 05-sass-js-bundle/README.md
@@ -1,1 +1,12 @@
-SASS | Gulp
+# Sass, JS modules, Gulp
+
+## Gulp 
+
+Установка Gulp CLI
+
+    npm install -g gulp-cli   
+
+Разрешить запуск скриптов PowerShell
+
+    Set-ExecutionPolicy RemoteSigned
+
--- 05-sass-js-bundle/app.js
+++ 05-sass-js-bundle/app.js
@@ -7,16 +7,16 @@ require('dotenv').config();
 
 // Соединение с базой данных
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD
+	host: process.env.HOST,
+	database: process.env.DATABASE,
+	user: process.env.USERNAME,
+	password: process.env.PASSWORD
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.log(err);
-    }
+	if (err) {
+		console.log(err);
+	}
 });
 
 // Парсинг json
@@ -41,54 +41,54 @@ app.listen(3000);
  * Маршруты
  */
 app.get('/', (req, res) => {
-    connection.query("SELECT * FROM items", (err, data, fields) => {
-        if (err) {
-            console.log(err);
-        }
+	connection.query("SELECT * FROM items", (err, data, fields) => {
+		if (err) {
+			console.log(err);
+		}
 
-        res.render('home', {
-            'items': data,
-        });
-    });
+		res.render('home', {
+			'items': data,
+		});
+	});
 });
 
 app.get("/items", (req, res) => {
-    connection.query("SELECT * FROM items", (err, data, fields) => {
-        if (err) {
-            console.log(err);
-        }
+	connection.query("SELECT * FROM items", (err, data, fields) => {
+		if (err) {
+			console.log(err);
+		}
 
-        res.status(200).send(data);
-    });
+		res.status(200).send(data);
+	});
 });
 
 app.get('/items/:id', (req, res) => {
-    connection.query("SELECT * FROM items WHERE id=?", [req.params.id],
-        (err, data, fields) => {
-            if (err) {
-                console.log(err);
-            }
+	connection.query("SELECT * FROM items WHERE id=?", [req.params.id],
+		(err, data, fields) => {
+			if (err) {
+				console.log(err);
+			}
 
-            res.render('item', {
-                'item': data[0],
-            })
-        });
+			res.render('item', {
+				'item': data[0],
+			})
+		});
 });
 
 app.get('/add', (req, res) => {
-    res.render('add')
+	res.render('add')
 });
 
 app.post('/store', (req, res) => {
-    connection.query(
-        "INSERT INTO items (title, image) VALUES (?, ?)",
-        [[req.body.title], [req.body.image]],
-        (err, data, fields) => {
-            if (err) {
-                console.log(err);
-            }
+	connection.query(
+		"INSERT INTO items (title, image) VALUES (?, ?)",
+		[[req.body.title], [req.body.image]],
+		(err, data, fields) => {
+			if (err) {
+				console.log(err);
+			}
 
-            res.redirect('/');
-        }
-    );
+			res.redirect('/');
+		}
+	);
 });
