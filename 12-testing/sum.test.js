f3af456d1ff2c76603670ecc5e2d924a70c07c49
loginovw8 <loginovw8@gmail.com>
2023-07-07 23:05:26 +0300

    Lesson 12 modified

--- 12-testing/sum.test.js
+++ 12-testing/sum.test.js
@@ -1,41 +1,7 @@
 const axios = require('axios');
-const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
-
-test('object assignment', () => {
-    const data = { one: 1 };
-    data['two'] = 2;
-    expect(data).toEqual({ one: 1, two: 2 });
-});
-
-test('adding positive numbers is not zero', () => {
-    for (let a = 1; a < 10; a++) {
-        for (let b = 1; b < 10; b++) {
-            expect(a + b).not.toBe(0);
-        }
-    }
-});
-
-// Truthiness
-test('null', () => {
-    const n = null;
-    expect(n).toBeNull();
-    expect(n).toBeDefined();
-    expect(n).not.toBeUndefined();
-});
-
-test('zero', () => {
-    const z = 0;
-    expect(z).not.toBeNull();
-    expect(z).toBeDefined();
-    expect(z).not.toBeUndefined();
-});
-
-// Async
 test('data message is hello world', async () => {
     const response = await axios.get('http://localhost:3000/');
+    expect(response.status).toBe(200);
     expect(response.data.message).toBe('Hello, World!');
 });
